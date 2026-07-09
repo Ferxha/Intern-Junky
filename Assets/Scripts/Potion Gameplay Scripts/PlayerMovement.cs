506ae29d03f7306d0c1c04e8d927a7c1b6e01928
sanss02 <sieres.san98@gmail.com>
2026-07-09 09:59:40 -0600

    [Add] Visualiza cursor

--- Assets/Scripts/Potion Gameplay Scripts/PlayerMovement.cs
+++ Assets/Scripts/Potion Gameplay Scripts/PlayerMovement.cs
@@ -37,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
